16768d5c0e3cca6a2310a78161e04ff4bbc8c3e5
Munish Rajan <rajankumar34401@gmail.com>
2025-12-24 12:51:47 +0530

    Backend with MongoDB Vector Search and Gemini AI integration

--- src/routes/sopRoutes.js
+++ src/routes/sopRoutes.js
@@ -3,7 +3,7 @@
 // src/routes/sopRoutes.js
 import { Router } from "express";
 import multer from "multer";
-import { uploadSOP, deleteSOP, listSOPs } from "../controllers/sopController.js";
+import { uploadSOP, deleteSOP, listSOPs,askQuestion } from "../controllers/sopController.js";
 
 const router = Router();
 const upload = multer({
@@ -18,5 +18,8 @@ const upload = multer({
 router.post("/upload", upload.single("file"), uploadSOP);
 router.delete("/:name", deleteSOP);
 router.get("/list", listSOPs);
+
+
+router.post("/ask", askQuestion); 
 
 export default router;
